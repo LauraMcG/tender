74201f13bdc3156020a71eea2ffdb27627684b97
Laura McGinn <mcginn.laura@gmail.com>
2017-03-15 20:52:29 -0500

    added local storage functionality!

--- assets/michael.js
+++ assets/michael.js
@@ -14,8 +14,13 @@ $(document).ready(function () {
 		event.preventDefault();
 
 		console.log('submit button has been clicked');
+		//collecting the user responses
 		allergies = $('#allergies').val().trim();
 		recipeCount = $('#recipe-count').val();
+		//adding user responses to local storage
+		localStorage.clear();
+		localStorage.setItem('allergies', allergies);
+		localStorage.setItem('recipe count', recipeCount);
 
 	//collecting restrictions may be tricky if they're multiple choice.
 	//let's talk about what data we want to collect, and how we're collecting it.
@@ -32,16 +37,15 @@ $(document).ready(function () {
 	}
 
 
-	function recipeNo() {
-		console.log('Next recipe image/name would generate / be called from API');
-	}
+	// function recipeNo() {
+	// 	console.log('Next recipe image/name would generate / be called from API');
+	// }
 
-	function recipeYes() {
-		console.log('Either continues "swiping" or goes to the comparison page');
-		// Store approved recipe into comparison array.
-		// if comparison array.length < numRecipes: next recipe generates on swipe screen
-		// else if comparison array.length = numRecipes: go to comparison page
-	}
+	$('.no').on('click', function(event) { 
+		console.log('Gross, I work');
+		console.log(localStorage.getItem('allergies'));
+		
+	});
 
 //end document ready, end script
 });
